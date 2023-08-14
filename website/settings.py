1201d23df37387cf33276fcc0c8eda66a78149ee
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-08-14 16:33:16 -0400

    "*" Hosts

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6glo#m%y3c8vo(vwbzg(z=jpj*@8z_s)0w7wuc4=g00i$2y+8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
