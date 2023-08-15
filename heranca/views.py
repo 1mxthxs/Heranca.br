069c997c3c3e2f1c4f147f6e6ff1f885b8838092
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-08-15 13:46:26 -0400

    Responsive Test

--- heranca/views.py
+++ heranca/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    
     return render(request, 'heranca/herança.html')
+
+def index2(request):
+    return render(request, 'heranca/psychic-goggles-main/herança.html')
