7b4818b918de5e079dbf6c7fbc7000ba6b1fe566
Matheus Ricardo Oliveira Lima <96189591+1mxthxs@users.noreply.github.com>
2023-09-03 21:41:12 -0400

    Fixes

--- quizes/urls.py
+++ quizes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import (
     QuizListView,
-    quiz_view, 
-    quiz_data_view, 
-    save_quiz_view,
+    quiz_view,
     )
 
 app_name = "quizes"
@@ -11,6 +9,4 @@ app_name = "quizes"
 urlpatterns = [
     path('', QuizListView.as_view(), name='main-view'),
     path('<pk>/', quiz_view, name="quiz-view"),
-    path('<pk>/save/', save_quiz_view, name="save-view"),
-    path('<pk>/data/', quiz_data_view, name="quiz-data-view"),
 ]
